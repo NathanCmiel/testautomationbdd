c27ee91db943e87e0bdc5f337a242518939f857a
Nathan Cmiel <cetan25@gmail.com>
2021-05-02 13:54:05 -0400

    implemented negative test steps in TrelloLoginSteps class

--- features/trello_login.feature
+++ features/trello_login.feature
@@ -1,8 +1,18 @@
+@smoke
 Feature: Trello login functionality
 
+@bob
   Scenario: User can login to Trello with valid credentials
     Given User goes to the Trello homepage
     And User clicks on login link
     When User is on login page
     And User logs in with valid credentials
     Then User should be able to see Trello boards page
+
+@dev
+  Scenario: User cannot login to Trello with invalid credentials
+    Given User goes to the Trello homepage
+    And User clicks on login link
+    When User is on login page
+    And User logs in with invalid credentials
+    Then User should be able to see an error message
--- src/test/java/stepdefinitions/TrelloLoginSteps.java
+++ src/test/java/stepdefinitions/TrelloLoginSteps.java
@@ -6,6 +6,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import com.github.javafaker.Faker;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -18,9 +20,9 @@ public class TrelloLoginSteps {
     private WebDriver driver;
 
     private void pause(int second) {
-        try{
+        try {
             Thread.sleep(second * 1000);
-        }catch(InterruptedException ignore){
+        } catch (InterruptedException ignore) {
             // do nothing
         }
     }
@@ -63,5 +65,30 @@ public class TrelloLoginSteps {
         WebElement pageBanner = Hooks.driver().findElement(loc_page_banner);
         boolean isVisible = pageBanner.isDisplayed();
         Assert.assertTrue(isVisible);
+    }
+
+    @When("^User logs in with invalid credentials$")
+    public void user_logs_in_with_invalid_credentials() {
+        By loc_email = By.id("user");
+        By loc_pass = By.id("password");
+        By loc_log_bttn = By.id("login");
+        WebElement emailInput = Hooks.driver().findElement(loc_email);
+        WebElement passInput = Hooks.driver().findElement(loc_pass);
+        Faker faker = new Faker();
+        String fakeEmail = faker.internet().emailAddress();
+        String fakePass = faker.internet().password();
+        emailInput.sendKeys((fakeEmail));
+        passInput.sendKeys(fakePass);
+        WebElement loginBttn = Hooks.driver().findElement(loc_log_bttn);
+        loginBttn.click();
+    }
+
+    @Then("^User should be able to see an error message$")
+    public void user_should_be_able_to_see_an_error_message() {
+        pause(3);
+        By loc_error_msg = By.xpath("//div[@id='error']/p");
+        WebElement errorMsgElement = Hooks.driver().findElement(loc_error_msg);
+        String actualErrorMessage= errorMsgElement.getText();
+        Assert.assertEquals("There isn't an account for this email", actualErrorMessage);
     }
 }
